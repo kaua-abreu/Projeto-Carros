612144096d0a7edc0c23adf658c47d66731e7159
kaua-abreu <kaua808player@gmail.com>
2026-08-24 10:42:35 -0300

    Script roteiro

---
+++ script_roteiro.R
@@ -0,0 +1,14 @@
+# -- Leitura e manipulação do banco de dados (main)
+
+# -- Análise descritiva (branch)
+# Histograma de consumo
+# Histograma de peso
+# Percentil de consumo
+# Percentil de peso
+# Média de consumo
+# Média de peso
+# Desvio-padrão de consumo
+# Desvio-padrão de peso
+
+# -- Análise inferencial (branch)
+# Teste ANOVA de comparação de médias para os tipos de carros
